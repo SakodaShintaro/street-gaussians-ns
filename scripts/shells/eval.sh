c82c12378839bce9f072886b432db21a5a5d4953
Shintaro Sakoda <sakoda@keio.jp>
2024-06-29 14:17:44 +0900

    Added render_output_path
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- scripts/shells/eval.sh
+++ scripts/shells/eval.sh
@@ -2,4 +2,5 @@ config_path=$1
 cuda_id=$2
 
 CUDA_VISIBLE_DEVICES=$cuda_id sgn-eval \
-    --load-config $config_path
+    --load-config $config_path \
+    --render_output_path $(dirname $config_path)/eval_render_output
