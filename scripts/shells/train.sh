eca46fb207061b109521d18446aa6361c095ffe6
Shintaro Sakoda <sakoda@keio.jp>
2024-07-14 09:44:44 +0900

    Fixed train.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- scripts/shells/train.sh
+++ scripts/shells/train.sh
@@ -1,19 +1,17 @@
 data_root=$1
-cuda_id=$2
+cuda_id=0
 
 mkdir -p output/
 
 CUDA_VISIBLE_DEVICES=$cuda_id  sgn-train street-gaussians-ns \
     --experiment_name street-gaussians-ns \
-    --output_dir output/ \
+    --output_dir $data_root/../output/ \
     --vis viewer+tensorboard \
     --viewer.quit_on_train_completion True \
     colmap-data-parser-config \
     --data $data_root \
     --colmap_path colmap/sparse/0 \
-    --load_3D_points True \
+    --load_3D_points False \
     --max_2D_matches_per_3D_point 0 \
-    --undistort True \
-    --segments-path segs \
-    --filter_camera_id 1 \
-    --init_points_filename points3D_withlidar.txt
+    --undistort False \
+    --init_points_filename points3D.txt
